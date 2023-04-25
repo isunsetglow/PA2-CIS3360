2a2b3f6b7d6f8ad81c0c098bfb639610f90c2acf
Maria <96081437+isunsetglow@users.noreply.github.com>
2023-04-25 01:53:42 -0400

    Update pa02.java

--- pa02.java
+++ pa02.java
@@ -179,11 +179,3 @@ public class pa02 {
         }
     }
 }
-/*=============================================================================
-| I Maria Hernandez (5336239) affirm that this program is
-| entirely my own work and that I have neither developed my code together with
-| any another person, nor copied any code from any other person, nor permitted
-| my code to be copied or otherwise used by any other person, nor have I
-| copied, modified, or otherwise used programs created by others. I acknowledge
-| that any violation of the above terms will be treated as academic dishonesty.
-+============================================================================*/
